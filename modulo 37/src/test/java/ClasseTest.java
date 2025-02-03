0cbc65ff25a329caaeedc5f43b4316c19714ec41
Daniel Augusto <danielaugusto.8@outlook.com>
2025-02-03 06:21:41 -0300

    Atividade Modulo 37

---
+++ modulo 37/src/test/java/ClasseTest.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class ClasseTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("Daniel"));
+    }
+}
